3172b0b7c09aa124a9bacfd340e853cebf9e07bf
Jose <divina.familia.nostri@gmail.com>
2025-01-24 21:09:56 -0600

    Create initials.cpp and add both initials to file

---
+++ initials.cpp
@@ -0,0 +1,15 @@
+// I am a coder
+#include <iostream>
+
+int main() {
+
+  std::cout <<"JJJJJ    QQQ \n";
+  std::cout <<"  J     Q   Q\n";
+  std::cout <<"  J     Q   Q\n";
+  std::cout <<"  J     Q   Q\n";
+  std::cout <<"J J     Q   Q\n";
+  std::cout <<"J J     Q  Q\n";
+  std::cout <<" JJ      QQ Q\n";
+  return 0;
+
+}
